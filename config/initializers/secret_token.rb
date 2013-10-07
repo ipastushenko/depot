9e6cd89cc2174c0d679bb73acd545e6e330919aa
sevenbits <sevenbits@sevenbits-H55M-D2H.(none)>
2013-10-07 15:41:42 +0700

    add validation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '27baafd7055256bd0a40e510ebaf6e2dbe78c6b9e60a5485e83207fd5897887c555450d54df0ef5463268543f515f83ec88de8661111d880c430263612b53a1c'
